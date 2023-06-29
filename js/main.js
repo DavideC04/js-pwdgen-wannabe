d4d4637271af4f195d15c25d0886da116b5881a1
Davide Cangiano <cangiano.0497@gmail.com>
2023-06-29 14:44:13 +0200

    add the first bonus request

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 /* Chiedere all'utente:
 Il suo nome;
 Il suo cognome;
-Il suo colore preferito; */
+Il suo colore preferito;
+2 numeri */
 
 // inserisco le variabili con il prompt
 
@@ -11,12 +12,18 @@ let surname = prompt("Qual è il tuo cognome?");
 
 let favoriteColor = prompt("Qual è il tuo colore preferito?");
 
+let number1 = prompt("Digita un numero casuale")
+
+let number2 = prompt("Digita un altro numero")
+
+let finalNumber = number1 / number2
+
 let password = userName + surname + favoriteColor;
 
 // utilizzo un template literal e stampo sulla console
 
- console.log(`Il tuo nome è: ${userName}
+console.log(`Il tuo nome è: ${userName}
 Il tuo cognome è: ${surname}
 Il tuo colore preferito è: ${favoriteColor}
  
-La tua password è: ${password}21`);
+La tua password è: ${password}${finalNumber}`);
