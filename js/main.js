3f4cff85e0e523e7713764b03269ae51d866f26a
Davide Cangiano <cangiano.0497@gmail.com>
2023-06-29 15:13:34 +0200

    Printed info and pswd on html

--- js/main.js
+++ js/main.js
@@ -2,7 +2,14 @@
 Il suo nome;
 Il suo cognome;
 Il suo colore preferito;
-2 numeri */
+Scrivere sulla console :"nomecognomecolorepreferito21"
+
+Bonus:
+
+-Al posto del 21 finale, chiedete all'utente 2 numeri e concatenate al posto del 21 finale, il primo numero diviso il secondo.
+-Mostrare la password generata sulla vostra pagina HTML sbizzarrendosi con il layout e volendo potete usare Bootstrap
+-Fare un layout responsive  */
+
 
 // inserisco le variabili con il prompt
 
@@ -11,6 +18,8 @@ let userName = prompt("Qual è il tuo nome?");
 let surname = prompt("Qual è il tuo cognome?");
 
 let favoriteColor = prompt("Qual è il tuo colore preferito?");
+
+// aggiungo i 2 numeri del bonus con l'operazione di divisione
 
 let number1 = prompt("Digita un numero casuale")
 
@@ -27,3 +36,11 @@ Il tuo cognome è: ${surname}
 Il tuo colore preferito è: ${favoriteColor}
  
 La tua password è: ${password}${finalNumber}`);
+
+
+// Stampo le informazioni su HTML
+
+document.getElementById("firstName").innerHTML = userName;
+document.getElementById("lastName").innerHTML = surname;
+document.getElementById("userColor").innerHTML = favoriteColor;
+document.getElementById("userpwd").innerHTML = password + finalNumber;
